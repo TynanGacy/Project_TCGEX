95910ae874a1cc027639759dde37f1d6f0535603
Tynan Gacy <76602843+TynanGacy@users.noreply.github.com>
2026-05-16 18:30:56 -0400

    Revert "starting the overworld"

--- autoload/game_state_manager.gd
+++ autoload/game_state_manager.gd
@@ -9,10 +9,6 @@ var play_time: float = 0.0
 ## placeholder_state.tscn so it can display the correct label.
 var _pending_state_name: String = ""
 
-## Spawn marker name for the next overworld scene load. Consumed once by
-## the receiving map's _ready, then cleared. Empty means "use DefaultSpawn".
-var _pending_spawn_marker: StringName = &""
-
 
 func change_state(scene_path: String) -> void:
 	get_tree().change_scene_to_file(scene_path)
@@ -29,14 +25,3 @@ func go_to_placeholder(state_name: String) -> void:
 
 func get_pending_state_name() -> String:
 	return _pending_state_name
-
-
-func change_overworld_scene(scene_path: String, spawn_marker: StringName) -> void:
-	_pending_spawn_marker = spawn_marker
-	change_state(scene_path)
-
-
-func consume_pending_spawn_marker() -> StringName:
-	var marker: StringName = _pending_spawn_marker
-	_pending_spawn_marker = &""
-	return marker
